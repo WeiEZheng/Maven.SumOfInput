9e896253c8cc2fb6b23f4f04d92764c35d4fea4e
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-02-23 23:29:27 -0500

    wrote lines to get integer from user and add the natural numbers up to that number and return the sum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
- * Created by iyasuwatts on 10/17/17.
+ *
  */
 public class Main {
 
     public static void main(String[] args){
-
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Enter an integer:"); //prompt for input
+        int N = Scan.nextInt(); //gets an integer from user
+        System.out.println(sumToN(N)); //calls method and prints result
+    }
+    public static Integer sumToN(Integer N){
+        int sum = 0, i=1;
+        while (i<=N){
+            sum += i; // add i to the rolling sum
+            i++;
+        }
+        return sum;
     }
 }
