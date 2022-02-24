c655edc76292ea2f728bdc6526e9394da261c6ce
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-02-24 14:49:13 -0500

    made main object, closed scanner, and changed println message. '

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+        Main Nsum = new Main();
         Scanner Scan = new Scanner(System.in);
         System.out.println("Enter an integer:"); //prompt for input
         int N = Scan.nextInt(); //gets an integer from user
-        System.out.println(sumToN(N)); //calls method and prints result
+        System.out.println("The sum of 1 to "+N+" is "
+                +nsum.sumToN(N)); //calls method and prints result
+        Scan.close();
     }
-    public static Integer sumToN(Integer N){
+    public Integer sumToN(Integer N){
         int sum = 0, i=1;
         while (i<=N){
             sum += i; // add i to the rolling sum
